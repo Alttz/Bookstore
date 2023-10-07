7dff6b99c900c27580b158c190340488207d753a
Antton Selkänen <antton.selkanen@gmail.com>
2023-10-07 18:38:54 +0300

    updated WebSecurityConfig

--- src/main/java/s23/Bookstore/WebSecurityConfig.java
+++ src/main/java/s23/Bookstore/WebSecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 
 import s23.Bookstore.web.UserDetailServiceImpl;
 
@@ -19,11 +22,24 @@ public class WebSecurityConfig {
 	
 	@Autowired
 	private UserDetailServiceImpl userDetailsService;
+	
+	private static final AntPathRequestMatcher[] WHITE_LIST_URLS = {
+			new AntPathRequestMatcher("/h2-console/**"),
+			new AntPathRequestMatcher("/book/**"),
+			new AntPathRequestMatcher("/books/**"),
+			new AntPathRequestMatcher("/api/**") };
+	
+	private static final AntPathRequestMatcher[] ADMIN_LIST_URLS = {
+			new AntPathRequestMatcher("/delete/**") };
 
+			
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(authorize -> authorize
+		http.authorizeHttpRequests(authorizeRequest -> authorizeRequest
 				// .requestMatchers("/index").permitAll()
+				.requestMatchers(antMatcher("/css/**")).permitAll()
+				.requestMatchers(antMatcher("/book/**")).permitAll()
+				.requestMatchers(WHITE_LIST_URLS).permitAll().requestMatchers(ADMIN_LIST_URLS).hasAuthority("ADMIN")
 				.anyRequest().authenticated()).formLogin(formlogin -> formlogin
 						// .loginPage("/login")
 						.defaultSuccessUrl("/booklist", true).permitAll())
--- src/main/java/s23/Bookstore/web/BookRestController.java
+++ src/main/java/s23/Bookstore/web/BookRestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,38 @@ public class BookRestController {
 	@GetMapping(value = "/book/{id}")
 	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long id) {
 		return repository.findById(id);
+	}
+	
+	@PutMapping(value = "/book/{id}")
+	public Book updateBook(@PathVariable("id") Long id, @RequestBody Book updatedBook) {
+	    // Find the existing book by ID
+	    Optional<Book> optionalBook = repository.findById(id);
+	    
+	    if (!optionalBook.isPresent()) {
+	        // Handle not found, throw an exception or return a custom response.
+	        throw new RuntimeException("Book not found with ID: " + id);
+	    }
+	    
+	    Book existingBook = optionalBook.get();
+	    
+	    // Update the fields of the existing book with the values from updatedBook
+	    existingBook.setTitle(updatedBook.getTitle());
+	    existingBook.setAuthor(updatedBook.getAuthor());
+	    existingBook.setPublicationYear(updatedBook.getPublicationYear());
+	    existingBook.setIsbn(updatedBook.getIsbn());
+	    existingBook.setPrice(updatedBook.getPrice());
+
+	    // If the category is provided in the updatedBook, you can also update it.
+	    if(updatedBook.getCategory() != null) {
+	        String categoryName = updatedBook.getCategory().getName();
+	        List<Category> categoryList = crepository.findByName(categoryName);
+	        Category categoryToUpdate = categoryList.isEmpty() ? new Category(categoryName) : categoryList.get(0);
+	        crepository.save(categoryToUpdate);
+	        existingBook.setCategory(categoryToUpdate);
+	    }
+	    
+	    // Save the updated book back to the database
+	    return repository.save(existingBook);
 	}
 	
 	@PostMapping(value = "/book")
